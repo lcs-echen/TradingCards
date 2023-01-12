02c0b082aa21d8914e11c75c0e38ae830b476e08
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-12 14:16:40 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Evelyn Chen on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
