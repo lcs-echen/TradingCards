66f67c0682b1d95fee466ae2094652f04851d55b
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-14 21:48:31 -0500

    Almost finished the second interface

--- TradingCards/TradingBackView.swift
+++ TradingCards/TradingBackView.swift
@@ -13,7 +13,6 @@ struct TradingBackView: View {
             BackgroundView()
             
             //Layer 4
-            
             RoundedRectangle(cornerRadius: 10)
                 .foregroundColor(.white)
                 .frame(width: 335, height: 640)
@@ -24,89 +23,120 @@ struct TradingBackView: View {
                 .padding(.bottom, 10)
                 .padding(.leading, 26)
             
-            
             //Layer 5
-            VStack {
-                VStack (alignment: .center){
-                    Text("NAME")
-                        .font(.title)
-                    
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image("FAG1")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(height: 160.0)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
-                            VStack(alignment: .leading){
-                                Group {
-                                    Text("POSITION   :")
-                                        .padding(.bottom, 5.0)
-                                        .font(.subheadline)
-                                    + Text("Position").font(.subheadline)
-                        
-                                }
-                                Text("AGE             : ")
-                                    .padding(.bottom, 5.0)
+            VStack (alignment: .center){
+                Text("MILLY BEK")
+                    .font(.title2.width(.expanded))
+                    .padding(.trailing, 135)
+                    .foregroundColor(Color("DarkRed"))
+                    .fontWeight(.semibold)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image("FAG1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 115, height: 160.0)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                        VStack(alignment: .leading){
+                            Group{
+                                Text("POSITION : ")
                                     .font(.subheadline)
-                                Text("HEIGHT       : ")
-                                    .font(.subheadline)
-                                    .padding(.bottom, 5.0)
-                                Text("LEAGUE      : ")
-                                    .font(.subheadline)
-                                    .padding(.bottom, 5.0)
-                                Text("COACH       : ")
-                                    .font(.subheadline)
-                                    .padding(.bottom, 5.0)
+                                + Text("HERE")
+                                    .font(.subheadline.width(.expanded))
+                                    .fontWeight(.semibold)
                             }
-                            .frame(width: 150)
+                            .padding(.bottom, 5)
+                            Group{
+                                Text("AGE           : ")
+                                    .font(.subheadline.width(.expanded))
+                                
+                                + Text("HERE")
+                                    .font(.subheadline.width(.expanded))
+                                    .fontWeight(.semibold)
+                            }
+                            .padding(.bottom, 5.0)
                             
+                            Group{
+                                Text("HEIGHT     : ")
+                                    .font(.subheadline)
+                                + Text("HERE")
+                                    .font(.subheadline.width(.expanded))
+                                    .fontWeight(.semibold)
+                            }
+                            .padding(.bottom, 5.0)
+                            
+                            
+                            Group{
+                                Text("LEAGUE    : ")
+                                    .font(.subheadline)
+                                
+                                + Text("USA")
+                                    .font(.subheadline.width(.expanded))
+                                    .fontWeight(.semibold)
+                            }
+                            .padding(.bottom, 5.0)
+                            
+                            Group{
+                                Text("COACH  :  ")
+                                    .font(.subheadline)
+                                
+                                + Text("Karch Kiraly")
+                                    .font(.subheadline.width(.expanded))
+                                    .fontWeight(.semibold)
+                            }
+                            .padding(.bottom, 5.0)
                         }
-                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
-                            .font(.caption)
-                            .padding(.top, 8.0)
-                    }
-                    
-                    .frame(width: 320)
-                    .padding(.leading, 25)
-                    
-                    Divider()
-                    Text("Name")
-                        .font(.largeTitle)
-                    
-                    Text("Volleyball Player")
-                        .font(.headline)
-                    Text("_______________")
-                        .font(.caption)
-                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-                        .font(.caption)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 280.0, height: 160.0)
-                        .padding(.leading, 20.0)
-                        .lineSpacing(/*@START_MENU_TOKEN@*/11.0/*@END_MENU_TOKEN@*/)
+                        .frame(width: 200)
                         
+                    }
+                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
+                        .font(.caption)
+                        .padding(.top, 8.0)
                 }
+                .frame(width: 320)
+                .padding(.leading, 35)
+                
+                Divider()
+                Text("MILLY BEK")
+                    .font(.largeTitle.width(.compressed))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("DarkRed"))
+                
+                Text("Volleyball Player")
+                    .font(.headline)
+                Text("_______________")
+                    .font(.caption)
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                    .font(.caption)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 280.0, height: 160.0)
+                    .padding(.leading, 20.0)
+                    .lineSpacing(/*@START_MENU_TOKEN@*/11.0/*@END_MENU_TOKEN@*/)
+                
             }
             .padding(.bottom, 90)
-                //Layer 6
-                
-                Image("USA")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 170)
-                    .padding(.top, 535)
-                    .padding(.trailing, 110)
-                
-                Image("Image")
-                    .resizable()
-                    .frame(width: 70, height: 70)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 75)
-                            .stroke(Color.black, lineWidth: 4.5)
-                    )
-                    .padding(.top, 530)
-                    .padding(.leading, 240)
-            }
+            
+            
+            //Layer 6
+            
+            Image("USA")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 170)
+                .padding(.top, 535)
+                .padding(.trailing, 110)
+            
+            Image("Image")
+                .resizable()
+                .frame(width: 70, height: 70)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 75)
+                        .stroke(Color.black, lineWidth: 4.5)
+                )
+                .padding(.top, 530)
+                .padding(.leading, 240)
+        }
         
     }
 }
